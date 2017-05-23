7f4298deb94f83b7652fa15be4c1d22aa48ae3ca
chenjianbo_007 <chenjianbo_007@sina.com>
2017-05-23 22:50:04 +0800

    asyn user init

--- app/controllers/wechat.js
+++ app/controllers/wechat.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const wechat = require('wechat');
 const jssdk = require('../libs/jssdk');
+const User = mongoose.model('User');
 
 module.exports = function (app) {
   app.use('/wechat', router);
@@ -31,15 +32,68 @@ const config = {
     appid: 'wx59a6cbb2a20d1069',
 };
 
+
 const handleWechatRequest = wechat(config, function(req,res,next){
 
     const message = req.weixin;
+    console.log(message, req.query);
 
-    console.log(message);
+    if(message.MsgType == 'event' && message.Content == 'subcribe'){
+        const newUser = new User({
+            openid: req.query.openid,
+            createAt: new Date(),
+            conversationCount: 0,
+        });
+
+        newUser.sve(function(err, user){
+            //do business
+        });
+    }
 
     res.reply('Hello');
 
 });
 
+const handleUserRequest = function(req, res, next){
+
+    const openid = req.query.openid;
+
+    if(!openid){
+        return next();
+    }
+
+    User.findOne({openid}).exec(function(err,user){
+        if(err){
+            return next(err);
+        }
+
+        if(user){
+            console.log('use existing user:', user)
+            req.user = user;
+            return next();
+
+        }
+
+        console.log(`create new user ${openid}`);
+        const newUser = new User({
+            openid,
+            createAt: new Date(),
+            conversationCount: 0,
+        });
+
+        newUser.save(function(e, u){
+            if(e){
+                return next();
+            }
+
+            req.user = u;
+            next();
+        });
+    });
+
+} 
+
+
+
 router.get('/conversation',handleWechatRequest);
-router.post('/conversation',handleWechatRequest);
+router.post('/conversation',handleUserRequest);
